11cad63862fc85625f9c7a3b29a0fa55de340994
xiaoer <xiaoer@example.com>
2019-10-19 12:52:21 +0800

    修改 金币不足弹窗增加api判断

--- page/SangongPage.ts
+++ page/SangongPage.ts
@@ -129,7 +129,7 @@ module gamesangong.page {
 			this._game.alert(StringU.substitute("老板，您的金币少于{0}哦~\n补充点金币去大杀四方吧~", limit), () => {
 				this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
 			}, () => {
-			}, true);
+			}, true,Tips.TIPS_SKIN_STR["cz"]);
 		}
 
 		private initPlayerInfo(): void {
